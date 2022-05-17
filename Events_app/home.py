050d9c4d60be9fd8541df97c64ef45f8ff5410a7
Ahmed gbr <studahmed91@gmail.com>
2022-05-17 18:05:41 +0200

    2nd commit album

--- Events_app/home.py
+++ Events_app/home.py
@@ -30,9 +30,10 @@ class Event(db.Model):
 	end_date =  db.Column(db.Date,unique=False,nullable=False,default=datetime.utcnow)
 	category =  db.Column(db.String(50),unique=False,nullable=False)
 	image_file =  db.Column(db.String(100),unique=False,nullable=False,default="default.jpg")
+	album_file =  db.Column(db.String(1000),unique=False,nullable=False,default="default.jpg")
 
 	def __repr__(self):
-		return f"name:{self.name}, content:{self.content},category:{self.category},img:{self.image_file}"
+		return f"name:{self.name}, content:{self.content},category:{self.category},img:{self.image_file},album:{self.album_file}"
 
 # ----------------------
 
@@ -60,15 +61,36 @@ def upload_image():
 	form = Add_event()
 	if form.validate_on_submit():
 	    file = request.files['photo']
+	    photo_album = request.files.getlist('photo')[1:]
+	    album = ""
+	    for i in range(len(photo_album)):
+	    	if(i == 0):
+	    		album+=photo_album[i].filename
+	    	else:
+	    		album+= "," + photo_album[i].filename
+
+
+
+	    print(album.split(','))
+
 	    event = Event(name=form.name.data,address=form.address.data,content=form.content.data,
-	    	category=form.category.data,image_file=file.filename,start_date=form.startDate.data,end_date=form.endDate.data)
+	    	category=form.category.data,image_file=file.filename,
+	    	start_date=form.startDate.data,end_date=form.endDate.data,album_file=album)
+
 	    db.session.add(event)
 	    db.session.commit()
 	    if file :
 	        filename = secure_filename(file.filename)
 	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+	        for photo in photo_album:
+	        	if photo:
+	        		photoname = secure_filename(photo.filename)
+	        		photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photoname))
+
 	        #print('upload_image filename: ' + filename)
 	        flash(f'Event created successfully','success')
+
 
 	        return redirect(url_for('home'))
 	    else:
@@ -81,11 +103,13 @@ def update_event(event_id):
 	form = Add_event()
 
 	if form.validate_on_submit():
+		file = request.files['photo']
+
 		event.name = form.name.data
 		event.contnet = form.content.data
 		event.address = form.address.data
 		event.category = form.category.data
-		#event.image_file = form.photo.data
+		event.image_file = file.filename
 		event.start_date = form.startDate.data
 		event.end_date = form.endDate.data
 
@@ -98,7 +122,7 @@ def update_event(event_id):
 		form.content.data = event.content
 		form.address.data = event.address
 		form.category.data = event.category
-		#form.photo.data = event.image_file
+		form.photo.data = event.image_file
 		form.startDate.data = event.start_date
 		form.endDate.data = event.end_date
 
@@ -115,6 +139,12 @@ def delete_event(event_id):
 	return redirect(url_for('home'))
 
 
+
+@app.route('/<int:event_id>/show_event')
+def show_event(event_id):
+	event = Event.query.get_or_404(event_id)
+	
+	return render_template('event_album.html',title='album',event=event)
 
 
 if __name__ == '__main__':
